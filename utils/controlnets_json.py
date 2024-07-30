87b0930bc90392daaa25f46255439e04e9db2d94
Liu Liu <i@liuliu.me>
2024-07-30 15:35:24 -0400

    Add 8-bit ControlNet models.

--- utils/controlnets_json.py
+++ utils/controlnets_json.py
@@ -33,6 +33,38 @@ def collect_metadata_from_list(file_path):
           for key, value in converted.items():
             sha256_dict[key] = value
           metadata_array.append(copy.deepcopy(metadata))
+          # Check if there are 8-bit counterpart.
+          if file.endswith('_f16.ckpt'):
+            # Append the 8-bit metadata if available.
+            q6p_q8p_file = file[:-len('_f16.ckpt')] + '_q6p_q8p.ckpt'
+            if q6p_q8p_file in converted:
+              metadata['file'] = q6p_q8p_file
+              metadata['name'] = metadata['name'] + ' (8-bit)'
+              # Update other fields have reference to this file.
+              for k, v in metadata.items():
+                if v == file:
+                  metadata[k] = q6p_q8p_file
+              metadata_array.append(copy.deepcopy(metadata))
+            else:
+              q8p_file = file[:-len('_f16.ckpt')] + '_q8p.ckpt'
+              if q8p_file in converted:
+                metadata['file'] = q8p_file
+                metadata['name'] = metadata['name'] + ' (8-bit)'
+                # Update other fields have reference to this file.
+                for k, v in metadata.items():
+                  if v == file:
+                    metadata[k] = q8p_file
+                metadata_array.append(copy.deepcopy(metadata))
+          elif file.endswith('_q8p.ckpt'):
+            q5p_file = file[:-len('_q8p.ckpt')] + '_q5p.ckpt'
+            if q5p_file in converted:
+              metadata['file'] = q5p_file
+              metadata['name'] = metadata['name'] + ' (8-bit)'
+              # Update other fields have reference to this file.
+              for k, v in metadata.items():
+                if v == file:
+                  metadata[k] = q5p_file
+              metadata_array.append(copy.deepcopy(metadata))
   return metadata_array, sha256_dict
 
 # Replace 'models.txt' with the path to your actual file if it's located elsewhere
